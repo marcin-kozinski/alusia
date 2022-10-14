ae22dd9a0fb991a96530d71152b552d51907978a
Marcin Koziński <marcin@getpocket.com>
2022-10-14 13:15:47 +0200

    Initial app version

---
+++ app/src/main/java/dev/kozinski/alusia/ui/theme/Color.kt
@@ -0,0 +1,34 @@
+package dev.kozinski.alusia.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple10 = Color(0xFF191249)
+val Purple20 = Color(0xFF2F295F)
+val Purple30 = Color(0xFF454077)
+val Purple40 = Color(0xFF5D5791)
+val Purple80 = Color(0xFFC7BFFF)
+val Purple90 = Color(0xFFE4DFFF)
+
+val PurpleGrey10 = Color(0xFF1B192C)
+val PurpleGrey20 = Color(0xFF302E41)
+val PurpleGrey30 = Color(0xFF474459)
+val PurpleGrey40 = Color(0xFF5F5C71)
+val PurpleGrey80 = Color(0xFFC8C3DC)
+val PurpleGrey90 = Color(0xFFE4DFF9)
+
+val Pink10 = Color(0xFF301122)
+val Pink20 = Color(0xFF482537)
+val Pink30 = Color(0xFF613B4E)
+val Pink40 = Color(0xFF7B5266)
+val Pink80 = Color(0xFFECB8CF)
+val Pink90 = Color(0xFFFFD8E8)
+
+val Grey10 = Color(0xFF1C1B1F)
+val Grey20 = Color(0xFF313034)
+val Grey30 = Color(0xFF47464F)
+val Grey50 = Color(0xFF77757E)
+val Grey60 = Color(0xFF928F99)
+val Grey80 = Color(0xFFC9C5D0)
+val Grey90 = Color(0xFFE5E1E6)
+val Grey95 = Color(0xFFF4EFF4)
+val Grey99 = Color(0xFFFFFBFF)
