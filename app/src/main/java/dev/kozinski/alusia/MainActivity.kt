900e1f95a39b495bae879c7c0c1c7e4ba46c5161
Marcin Koziński <marcin@getpocket.com>
2022-11-04 18:23:30 +0100

    Switch to Material date range picker

--- app/src/main/java/dev/kozinski/alusia/MainActivity.kt
+++ app/src/main/java/dev/kozinski/alusia/MainActivity.kt
@@ -1,14 +1,11 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
-
 package dev.kozinski.alusia
 
-import android.app.DatePickerDialog
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
@@ -17,10 +14,12 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.util.component1
+import androidx.core.util.component2
+import com.google.android.material.datepicker.MaterialDatePicker
 import dev.kozinski.alusia.ui.theme.AlusiaTheme
 import java.time.Clock
 import java.time.Instant
@@ -28,14 +27,30 @@ import java.time.LocalDate
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
-import java.util.*
+import java.util.Locale
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val clock = Clock.systemDefaultZone()
+            val mainScreenState = rememberMainScreenState(
+                start = LocalDate.now(clock).plusDays(1),
+                end = LocalDate.now(clock).plusDays(1)
+            )
             AlusiaTheme {
                 MainScreen(
+                    onDateRangeFieldClick = {
+                        MaterialDatePicker.Builder.dateRangePicker()
+                            .build()
+                            .apply {
+                                addOnPositiveButtonClickListener { (start, end) ->
+                                    mainScreenState.start = localDateOfEpochMilli(start)
+                                    mainScreenState.end = localDateOfEpochMilli(end)
+                                }
+                                show(supportFragmentManager, null)
+                            }
+                    },
                     onSendClick = {
                         try {
                             startActivity(
@@ -49,12 +64,12 @@ class MainActivity : ComponentActivity() {
                         } catch (e: ActivityNotFoundException) {
                             Toast.makeText(
                                 this,
-                                "Nie udało się znaleźć aplikacji email",
+                                "Nie udało się otworzyć aplikacji email",
                                 Toast.LENGTH_SHORT
                             ).show()
                         }
                     },
-                    clock = Clock.systemDefaultZone(),
+                    mainScreenState = mainScreenState,
                 )
             }
         }
@@ -63,8 +78,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(
+    onDateRangeFieldClick: () -> Unit,
     onSendClick: (String) -> Unit,
-    clock: Clock
+    mainScreenState: MainScreenState,
 ) {
     Scaffold(
         topBar = {
@@ -78,26 +94,20 @@ fun MainScreen(
         ) {
             Spacer(Modifier.height(Spacing.Base))
 
-            var start by remember { mutableStateOf(LocalDate.now(clock).plusDays(1)) }
-            var end by remember { mutableStateOf(LocalDate.now(clock).plusDays(1)) }
-            Row {
-                Spacer(Modifier.width(Spacing.Base))
-
-                DateField(
-                    value = start,
-                    onValueChange = { start = it },
-                    label = { Text("Od") },
+            Box(
+                Modifier
+                    .clickable { onDateRangeFieldClick() }
+                    .padding(horizontal = Spacing.Base)
+            ) {
+                TextFieldDefaults.OutlinedTextFieldDecorationBox(
+                    value = mainScreenState.displayRange,
+                    innerTextField = { Text(text = mainScreenState.displayRange) },
+                    enabled = true,
+                    singleLine = true,
+                    visualTransformation = VisualTransformation.None,
+                    interactionSource = MutableInteractionSource(),
+                    label = { Text("Od – do") },
                 )
-
-                Spacer(Modifier.width(Spacing.Base / 2))
-
-                DateField(
-                    value = end,
-                    onValueChange = { end = it },
-                    label = { Text("Do") },
-                )
-
-                Spacer(Modifier.width(Spacing.Base))
             }
 
             Spacer(Modifier.height(Spacing.Base / 2))
@@ -110,7 +120,7 @@ fun MainScreen(
 
             Spacer(Modifier.height(Spacing.Base / 2))
 
-            val emailBody = composeEmail(start, end)
+            val emailBody = composeEmail(mainScreenState.start, mainScreenState.end)
             SelectionContainer {
                 Text(
                     text = emailBody,
@@ -133,38 +143,21 @@ fun MainScreen(
     }
 }
 
-@Composable
-fun DateField(
-    value: LocalDate,
-    onValueChange: (LocalDate) -> Unit,
-    modifier: Modifier = Modifier,
-    label: @Composable (() -> Unit)? = null,
+class MainScreenState(
+    start: LocalDate,
+    end: LocalDate,
 ) {
-    val context = LocalContext.current
-    val picker = remember(context, value, onValueChange) {
-        DatePickerDialog(
-            context,
-            { _, year, month, day -> onValueChange(LocalDate.of(year, month + 1, day)) },
-            value.year,
-            value.monthValue - 1,
-            value.dayOfMonth
-        )
-    }
-    Box(
-        modifier
-            .clickable { picker.show() }
-    ) {
-        TextFieldDefaults.OutlinedTextFieldDecorationBox(
-            value = value.toString(),
-            innerTextField = { Text(text = value.toString()) },
-            enabled = true,
-            singleLine = true,
-            visualTransformation = VisualTransformation.None,
-            interactionSource = MutableInteractionSource(),
-            label = label,
-        )
-    }
+    var start by mutableStateOf(start)
+    var end by mutableStateOf(end)
+    val displayRange get() = "$start – $end"
+}
 
+@Composable
+fun rememberMainScreenState(
+    start: LocalDate,
+    end: LocalDate,
+) = remember(start, end) {
+    MainScreenState(start, end)
 }
 
 fun composeEmail(
@@ -194,22 +187,26 @@ fun composeEmail(
 }
 
 @Preview(
-    showBackground = true,
     showSystemUi = true
 )
 @Composable
 fun MainScreenPreview() {
     AlusiaTheme {
         MainScreen(
+            onDateRangeFieldClick = { },
             onSendClick = {},
-            clock = Clock.fixed(
-                Instant.EPOCH,
-                ZoneOffset.UTC
+            mainScreenState = rememberMainScreenState(
+                start = LocalDate.ofEpochDay(1),
+                end = LocalDate.ofEpochDay(1),
             ),
         )
     }
 }
 
 object Spacing {
-    val Base = 16.dp
+    val Base @Composable get() = 16.dp
+}
+
+fun localDateOfEpochMilli(epochMilli: Long, offset: ZoneOffset = ZoneOffset.UTC): LocalDate {
+    return Instant.ofEpochMilli(epochMilli).atOffset(offset).toLocalDate()
 }
