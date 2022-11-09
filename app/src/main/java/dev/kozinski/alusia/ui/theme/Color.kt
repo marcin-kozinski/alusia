0ee0fc1f52829e72f7515282d028b16c1b89dc40
Marcin Koziński <marcin@getpocket.com>
2022-11-09 18:24:41 +0100

    feat(theme): update color scheme and system bar colors

--- app/src/main/java/dev/kozinski/alusia/ui/theme/Color.kt
+++ app/src/main/java/dev/kozinski/alusia/ui/theme/Color.kt
@@ -2,33 +2,44 @@ package dev.kozinski.alusia.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple10 = Color(0xFF191249)
-val Purple20 = Color(0xFF2F295F)
-val Purple30 = Color(0xFF454077)
-val Purple40 = Color(0xFF5D5791)
-val Purple80 = Color(0xFFC7BFFF)
-val Purple90 = Color(0xFFE4DFFF)
+object ForgetMeNot {
+    val `10` = Color(0xFF001D33)
+    val `20` = Color(0xFF003353)
+    val `30` = Color(0xFF004A76)
+    val `40` = Color(0xFF00639B)
+    val `80` = Color(0xFF97CBFF)
+    val `90` = Color(0xFFCEE5FF)
+    val `100` = Color(0xFFFFFFFF)
+}
 
-val PurpleGrey10 = Color(0xFF1B192C)
-val PurpleGrey20 = Color(0xFF302E41)
-val PurpleGrey30 = Color(0xFF474459)
-val PurpleGrey40 = Color(0xFF5F5C71)
-val PurpleGrey80 = Color(0xFFC8C3DC)
-val PurpleGrey90 = Color(0xFFE4DFF9)
+object Sky {
+    val `10` = Color(0xFF0E1D2A)
+    val `20` = Color(0xFF243240)
+    val `30` = Color(0xFF3A4857)
+    val `40` = Color(0xFF51606F)
+    val `80` = Color(0xFFB9C8DA)
+    val `90` = Color(0xFFD5E4F7)
+    val `100` = Color(0xFFFFFFFF)
+}
 
-val Pink10 = Color(0xFF301122)
-val Pink20 = Color(0xFF482537)
-val Pink30 = Color(0xFF613B4E)
-val Pink40 = Color(0xFF7B5266)
-val Pink80 = Color(0xFFECB8CF)
-val Pink90 = Color(0xFFFFD8E8)
+object Heather {
+    val `10` = Color(0xFF231533)
+    val `20` = Color(0xFF392A49)
+    val `30` = Color(0xFF504061)
+    val `40` = Color(0xFF68587A)
+    val `80` = Color(0xFFD3BFE6)
+    val `90` = Color(0xFFEFDBFF)
+    val `100` = Color(0xFFFFFFFF)
+}
 
-val Grey10 = Color(0xFF1C1B1F)
-val Grey20 = Color(0xFF313034)
-val Grey30 = Color(0xFF47464F)
-val Grey50 = Color(0xFF77757E)
-val Grey60 = Color(0xFF928F99)
-val Grey80 = Color(0xFFC9C5D0)
-val Grey90 = Color(0xFFE5E1E6)
-val Grey95 = Color(0xFFF4EFF4)
-val Grey99 = Color(0xFFFFFBFF)
+object Neutral {
+    val `10` = Color(0xFF1B1B1B)
+    val `20` = Color(0xFF303030)
+    val `30` = Color(0xFF474747)
+    val `50` = Color(0xFF767676)
+    val `60` = Color(0xFF919191)
+    val `80` = Color(0xFFC6C6C6)
+    val `90` = Color(0xFFE2E2E2)
+    val `95` = Color(0xFFF1F1F1)
+    val `99` = Color(0xFFFCFCFC)
+}
